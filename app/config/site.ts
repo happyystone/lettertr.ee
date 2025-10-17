c453c2bd4fee63e61a6bd3dd8d7f538576e46061
growinjoy <happystone.kang@gmail.com>
2025-10-17 21:28:48 +0900

    feat: site.url 적용 안되는 이슈로 import.meta.env 테스트 적용

--- app/config/site.ts
+++ app/config/site.ts
@@ -1,17 +1,15 @@
-const site_url = import.meta.env.VITE_PUBLIC_APP_URL || 'http://localhost:5173';
-
 export const site = {
   name: 'Lettertree',
   description: '모든 뉴스레터를 한 곳에서, 더 깔끔하게.',
-  url: site_url,
+  url: import.meta.env.VITE_PUBLIC_APP_URL || 'http://localhost:5173',
   domain: 'lettertr.ee',
-  ogImage: `${site_url}/og.jpg`,
+  ogImage: `${import.meta.env.VITE_PUBLIC_APP_URL || 'http://localhost:5173'}/og.jpg`,
   favicon: '/favicon.ico',
   // favicon: `${site_url}/favicon.svg`,
-  googleFavicon: `${site_url}/favicon.png`,
+  googleFavicon: `${import.meta.env.VITE_PUBLIC_APP_URL || 'http://localhost:5173'}/favicon.png`,
   lightlogo: '/bluelogoinlightmode.svg', //'blacklogo.svg', //'/bluelogoinlightmode.svg',
   darkLogo: '/bluelogoindarkmode.svg', //'/bluelogoindarkmode.svg',
-  appleTouchIcon: `${site_url}/apple-touch-icon.png`,
+  appleTouchIcon: `${import.meta.env.VITE_PUBLIC_APP_URL || 'http://localhost:5173'}/apple-touch-icon.png`,
   mailSupport: 'support@lettertr.ee', // Support email address
   mailFrom: import.meta.env.MAIL_FROM || 'noreply@lettertr.ee', // Transactional email address
   links: {
